4526bf3e6619ef22554a6e775d74449aafdcb556
Nazik <nazzikmart@gmail.com>
2023-05-05 13:40:07 +0300

    new comit script 05/05/2023

--- src/Components/Costs/CostItem.js
+++ src/Components/Costs/CostItem.js
@@ -1,26 +1,18 @@
 import React from "react";
 import "./CostItem.css";
 import Card from "../UI/Card";
-import { useState } from "react";
-
 import CostDate from "./CostDate";
 
 export default function CostItem(props) {
-  const [description, setDescription] = useState(props.description);
-
-  function changeDescriptionHandler() {
-    setDescription("new cost");
-  }
   return (
     <Card className="cost-item">
       <div className="">
         <CostDate date={props.date} />
       </div>
       <div className="cost-item__description">
-        <h2>{description}</h2>
+        <h2>{props.description}</h2>
         <div className="cost-item__price">{props.amout} грн</div>
       </div>
-      <button onClick={changeDescriptionHandler}>Редагувати</button>
     </Card>
   );
 }
--- src/Components/Costs/Costs.js
+++ src/Components/Costs/Costs.js
@@ -2,25 +2,29 @@ import React from "react";
 import "./Costs.css";
 import CostItem from "./CostItem";
 import Card from "../UI/Card";
+import CostsFilter from "./CostsFilter";
+import { useState } from "react";
 
 export default function Costs(props) {
+  const [selectedYear, setSelectedYear] = useState("2023");
+  const yearChangeHandler = (year) => {
+    console.log(year);
+    console.log("costs");
+  };
   return (
-    <Card className="costs">
-      <CostItem
-        description={props.costs[0].description}
-        amout={props.costs[0].amout}
-        date={props.costs[0].date}
-      />
-      <CostItem
-        description={props.costs[1].description}
-        amout={props.costs[1].amout}
-        date={props.costs[1].date}
-      />
-      <CostItem
-        description={props.costs[2].description}
-        amout={props.costs[2].amout}
-        date={props.costs[2].date}
-      />
-    </Card>
+    <div>
+      <Card className="costs">
+        <CostsFilter year={selectedYear} onChangeYear={yearChangeHandler} />
+        {props.costs.map((cost) => {
+          return (
+            <CostItem
+              date={cost.date}
+              description={cost.description}
+              amout={cost.amout}
+            />
+          );
+        })}
+      </Card>
+    </div>
   );
 }
